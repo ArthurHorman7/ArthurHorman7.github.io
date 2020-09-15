0a66acde9d3b27e9e9ac03d7c0a2dbf82b39c42c
Arthur Horman de Medeiros Correia <67442332+ArthurHorman7@users.noreply.github.com>
2020-09-15 13:41:42 -0300

    Add files via upload

---
+++ Cinefilósofo/javascript.js
@@ -0,0 +1,66 @@
+
+    var fil = document.getElementById('filmesbtn')
+    var ser = document.getElementById('seriesbtn')
+    var exfil = document.getElementById('filmes')
+    var exser = document.getElementById('net')
+
+function clickF(fi) {
+    exser.innerHTML = ` &nbsp;&nbsp;O filme é uma obra de arte realizada através da sucessão de imagens em vídeo e com som. 
+    O cinema ou a arte de fazer filmes de diversos tipos é considerado uma das sete artes e é hoje em dia 
+    uma das mais populares, pois chega a uma importante e variada quantidade de público. 
+    Os filmes apresentam certas características gerais que 
+    podem variar entre um caso e outro.<br><br>
+    
+    &nbsp;&nbsp;A realização de um filme é dirigida por um diretor, a pessoa com maior hierarquia na hora de tomar decisões 
+    sobre a elaboração específica da obra. Ao mesmo tempo, o filme conta uma história expressada em forma de script ou guia. 
+    Este script é encenado por atores e atrizes que interpretam os personagens. Uma vez que este trabalho é realizado acontece 
+    a edição de imagens. O último passo de todo o processo tem a ver com a publicidade e a promoção do filme antes de sua 
+    exibição em cinemas e teatros.<br><br>
+    
+    &nbsp;&nbsp;Um filme se estrutura em base na sequência de imagens que são postas uma atrás da outra e que projetadas de maneira 
+    contínua, simulam movimento. Esta sucessão tem que ser maior que 18 pictogramas (ou pequenas imagens), pois de 
+    outro modo, a sequência de projeção aparece ao olho humano como um 
+    processo recortado e não contínuo.<br><br>
+
+    &nbsp;&nbsp;Independentemente das questões técnicas, um filme é sempre considerado uma obra de arte que exige um grande esforço 
+    de produção. Apesar de a atuação e a direção serem os elementos que mais se destacam, também são imprescindíveis o 
+    vestuário, a música, a fotografia, a criação de ambientes apropriados, a edição, o som, 
+    a utilização dos efeitos especiais, etc.<br><br>
+
+    &nbsp;&nbsp; Os filmes, uma vez terminados, costumam ser exibidos em cinemas ou teatros que recebem grandes quantidades de público. 
+    É aí que entra a parte comercial, pois em muitas ocasiões, um filme pode ser considerado bom de acordo com a 
+    quantidade de espectadores que têm, assim como o profundo fenômeno do merchandising, que são os que permitem 
+    às grandes companhias de cinema recuperar o investimento e ganhar milhões de dólares de lucro.<br><br>`
+
+    document.body.style.font = '15pt normal Georgia'
+    
+}
+
+function clickS(se) {
+    exfil.innerHTML = ` &nbsp;&nbsp; Normalmente, utilizamos o termo série para referir aos programas de televisão que envolvem 
+    uma continuidade na narração, assim como nas características argumentativas mais importantes. 
+    Este nome provém do significado específico do termo série, que envolve a existência de um 
+    conjunto de elementos com características comuns entrelaçadas de algum modo com o fim de 
+    estabelecer um nexo de continuidade entre todas elas. Hoje em dia, as séries televisivas contam 
+    com uma grande popularidade, especialmente nos países de língua inglesa.<br><br>
+    
+    &nbsp;&nbsp; O fenômeno das séries de televisão teve início há várias décadas, embora você possa dizer que não foi há décadas 
+    atrás que houve um boom destas séries e um desenvolvimento de novas histórias e shows em grandes números nos anos 
+    oitenta e noventa. Durante estas décadas, as séries de televisão de grande popularidade, em parte, devido ao 
+    aumento de espectadores em todo o mundo e podendo muitas delas não só consagrar-se no seu número de espectadores, 
+    mas também no que diz respeito à qualidade dos seus argumentos.<br><br>
+    
+    &nbsp;&nbsp; As séries de televisão contam com algumas características em comum. Por um lado, suas histórias se desenvolvem 
+    ao longo de diferentes capítulos de uma temporada (ou conjunto de episódios), e embora cada capítulo tenha um 
+    tema específico, os personagens, suas personalidades, os cenários e as situações são sempre os mesmos. Outra 
+    característica muito típica das séries de televisão é que são emitidas semanalmente pela produção e a execução 
+    dos projetos mais cotados do que aquelas que se levam adiante em caso de filmes ou longa metragens.<br><br>
+
+    &nbsp;&nbsp; Entre as séries de televisão mais populares encontramos as comédias, embora os dramas e as séries de suspense 
+    tenham sempre uma importante popularidade. Ao mesmo tempo, as séries podem ser documentários ou histórias 
+    desde que se trate de um tema comum ao longo de seus episódios.`
+
+    document.body.style.font = '15pt normal Georgia'
+
+
+}
