bbd05eb57a141d9727a455b6c9e05d4a9ff542ce
Arthur Horman de Medeiros Correia <67442332+ArthurHorman7@users.noreply.github.com>
2020-10-27 16:17:38 -0300

    Add files via upload

--- Caravan/js/script.js
+++ Caravan/js/script.js
@@ -1,1 +1,15 @@
-sss
+
+
+$(function () {     // Comentário ao passar o mouse
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+  
+
+
+
+ /*             Mudando a velocidade do CAROUSEL (SLIDES)
+ $('.carousel').carousel({
+    interval: 3000
+  })
+  
+*/
